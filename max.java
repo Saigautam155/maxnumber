892700849bb06ff0284996fec656f13fdb9d98e0
Saigautam155 <161446196+Saigautam155@users.noreply.github.com>
2024-06-07 14:42:55 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,21 @@
+class max
+{
+public static void main(String args[])
+{
+int a=10,b=56,c=2;
+int m=0;
+if (a>b && a>c)
+{
+m=a;
+}
+else if (b>c)
+{
+m=b;
+}
+else
+{
+m=c;
+}
+System.out.println(m);
+}
+}
